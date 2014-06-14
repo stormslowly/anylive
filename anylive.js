bad539af2bce8f33c280f50314e592108a6d18e4
stormslowly <stormslowly@gmail.com>
2014-06-14 22:57:41 +0800

    parse port number

--- anylive.js
+++ anylive.js
@@ -1,18 +1,14 @@
-'use strict';
+#!/usr/bin/env node
 /* global require, console, process */
+'use strict';
 var gaze = require('gaze');
 var tinylr = require('tiny-lr');
 var connect = require('connect');
 var livereload = require('connect-livereload');
-// var lr ={
-//   port:35729
-// };
-console.log(process.argv);
-return;
 
-var rlServer = new tinylr();
-rlServer.removeAllListeners('error');
-rlServer.on('error',function(err){
+var liveReloadServer = new tinylr();
+liveReloadServer.removeAllListeners('error');
+liveReloadServer.on('error',function(err){
   console.log('livereload server failed :',err);
   process.exit(1);
 });
@@ -32,21 +28,25 @@ staticServer.use(livereload({
 staticServer.use(connect.static(cwd));
 staticServer.use(connect.directory(cwd));
 
-
-gaze('*.json', function(err,watcher){
+gaze(['**/*.js',
+      '**/*.css',
+      '**/*html',
+      '!.git/**',
+      '!node_modules/**'
+      ], function(err,watcher){
   if(err) return console.log(err);
   console.log('big brother is watching..');
 
-  rlServer.listen(35729,function(err){
+  liveReloadServer.listen(35729,function(err){
     if(err) return console.log(err);
 
     console.log('second brother is watching..');
     watcher.on('all',function(e,files){
       console.log('files changed',files);
-      rlServer.changed({body:{files:files}});
+      liveReloadServer.changed({body:{files:files}});
     });
 
-    staticServer.listen(9090, function (err) {
+    staticServer.listen(staticServerPort, function (err) {
       if (err) return console.log('static Srever error',err);
         console.log('static Server is ready on port: '+staticServerPort);
     });
