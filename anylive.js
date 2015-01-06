20a27e882718c97a8a65237a81f7030866ea244b
stormslowly <stormslowly@gmail.com>
2015-01-06 14:09:05 +0800

    use the open package

--- anylive.js
+++ anylive.js
@@ -6,14 +6,8 @@ var gaze = require('gaze');
 var tinylr = require('tiny-lr');
 var connect = require('connect');
 var livereload = require('connect-livereload');
-var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
 var liveReloadServer = new tinylr();
-// liveReloadServer.removeAllListeners('error');
-// liveReloadServer.on('error',function(err){
-//   console.log('livereload server failed :',err);
-//   process.exit(1);
-// });
+var openURL = require('open');
 
 var cwd = process.cwd();
 var staticServerPort = parseInt(process.argv[2]) || 8080;
@@ -23,27 +17,6 @@ staticServer.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
-
-var openURL = function(url) {
-  switch (process.platform) {
-    case 'darwin':
-      exec('open ' + url);
-      break;
-    case 'win32':
-      exec('start ' + url);
-      break;
-    default:
-      spawn('xdg-open', [url]);
-      // I use `spawn` since `exec` fails on my machine (Linux i386).
-      // I heard that `exec` has memory limitation of buffer size of 512k.
-      // http://stackoverflow.com/a/16099450/222893
-      // But I am not sure if this memory limit causes the failure of `exec`.
-      // `xdg-open` is specified in freedesktop standard, so it should work on
-      // Linux, *BSD, solaris, etc.
-  }
-};
-
-
 
 staticServer.use(livereload({
   port: 35729
